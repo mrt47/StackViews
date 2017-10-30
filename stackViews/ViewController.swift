b43722ee09dfb42c6f990db502b74e6f37dd1ebd
Murat Kuran <muratkuran85@gmail.com>
2017-10-30 23:35:33 +0300

    Initial Commit

---
+++ stackViews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stackViews
+//
+//  Created by Murat Kuran on 30/10/2017.
+//  Copyright © 2017 Murat Kuran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
